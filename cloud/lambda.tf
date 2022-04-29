d093814276d3b20583c7ae21a0d83daa54c3e1ef
PatrickGHub <patrick@pkelly.co>
2022-04-29 15:53:39 +0100

    Update lambda hash

--- cloud/lambda.tf
+++ cloud/lambda.tf
@@ -9,7 +9,7 @@ resource "aws_lambda_function" "scraper" {
   runtime = "nodejs14.x"
   role = aws_iam_role.lambda_role.arn
 
-  source_code_hash = data.archive_file.scraper.output_base64sha256
+  source_code_hash = base64sha256(filesha256("scraper.zip"))
   filename = data.archive_file.scraper.output_path
   handler = "index.handler"
   timeout = 15
