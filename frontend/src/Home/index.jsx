1e881cbee565088f9997c6f16cc2ca92b710aa91
PatrickGHub <patrick@pkelly.co>
2022-05-20 13:04:44 +0100

    Sort VIN groups by most recent

--- frontend/src/Home/index.jsx
+++ frontend/src/Home/index.jsx
@@ -2,35 +2,38 @@ import data from '../cars.json'
 import PriceUpdatesTable from '../components/PriceUpdatesTable'
 
 const groupByVin = (data) => {
-  const formattedData = {}
+  const dataGroupedByVin = {}
 
   data.map((car) => {
-    if (!formattedData[car.car_id]) {
-      return formattedData[car.car_id] = [{ ...car }]
+    if (!dataGroupedByVin[car.car_id]) {
+      return dataGroupedByVin[car.car_id] = [{ ...car }]
     }
 
-    return formattedData[car.car_id] = [
-      ...formattedData[car.car_id],
+    dataGroupedByVin[car.car_id] = [
+      ...dataGroupedByVin[car.car_id],
       { ...car }
     ]
-
+    
+    return dataGroupedByVin[car.car_id].sort((a, b) => {
+      return new Date(b.date_found) - new Date(a.date_found)
+    })
   })
 
-  return formattedData
+  return dataGroupedByVin
 }
 
 const Home = () => {
-  const formattedData = groupByVin(data)
+  const dataGroupedByVin = groupByVin(data)
 
   return (
     <div>
       {
-        Object.keys(formattedData).map((carId) => {
+        Object.keys(dataGroupedByVin).map((carId) => {
           return (
             <div style={{marginBottom: '1em'}}>
               <p>{ carId }</p>
-              <p>{ formattedData[carId][0].model }</p>
-              <PriceUpdatesTable rows={formattedData[carId]} />
+              <p>{ dataGroupedByVin[carId][0].model }</p>
+              <PriceUpdatesTable rows={dataGroupedByVin[carId]} />
             </div>
           )
         })
